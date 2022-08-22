15d396d47b9c7910549eac0c6b8b5962a1887418
Thomas Risberg <risbergt@vmware.com>
2022-08-22 16:21:54 -0400

    Add Spring Cloud Function sample

---
+++ spring-cloud-function/src/test/java/com/example/helloapp/HelloAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
