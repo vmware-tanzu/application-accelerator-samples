84c1c33e9c82b67a1e441f6f604291a4de1f13d5
Sarah Walther <sawalther@vmware.com>
2022-09-06 17:40:37 -0400

    add appsso java accelerator
    Co-authored-by: Dmitriy Dubson <ddubson@vmware.com>

---
+++ appsso-starter-java/src/main/java/com/vmware/tanzu/apps/sso/accelerator/AppSSOAcceleratorApplication.java
@@ -0,0 +1,13 @@
+package com.vmware.tanzu.apps.sso.accelerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSSOAcceleratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSSOAcceleratorApplication.class, args);
+	}
+
+}
