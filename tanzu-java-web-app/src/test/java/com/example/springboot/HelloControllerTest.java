b9dc631e8d28f39482512be2328eada78d7d74ae
Kelwin Payares <kpayares@vmware.com>
2022-09-29 18:13:39 +0200

    add java-function and tanzu-java-web-app

---
+++ tanzu-java-web-app/src/test/java/com/example/springboot/HelloControllerTest.java
@@ -0,0 +1,31 @@
+package com.example.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HelloController.class)
+class HelloControllerTest {
+
+    @Autowired
+    private HelloController controller;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void index() throws Exception {
+        assertEquals("Greetings from Spring Boot + Tanzu!", controller.index());
+
+        mockMvc
+            .perform(get("/"))
+            .andExpect(status().isOk())
+            .andExpect(content().string("Greetings from Spring Boot + Tanzu!"));
+    }
+}
