857741c4e75ebf81d1ed3bf69ab177bf12ddd81f
Tim Hess <thess@vmware.com>
2022-10-28 14:05:07 -0500

    Update csharp apps to fix swagger when deployed, prevent scale to 0
    Let Steeltoe manage ports in Steeltoe app

--- weatherforecast-steeltoe/Program.cs
+++ weatherforecast-steeltoe/Program.cs
@@ -1,12 +1,10 @@
 using Microsoft.Extensions.FileProviders;
+using Steeltoe.Common.Hosting;
 using Steeltoe.Management.Endpoint;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// honor PORT var if set
-var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-var url = string.Concat("http://0.0.0.0:", port);
-builder.WebHost.UseUrls(url);
+builder.UseCloudHosting();
 
 // Learn more about management endpoints at https://docs.steeltoe.io/api/v3/management/
 builder.WebHost.AddAllActuators();
@@ -24,9 +22,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseFileServer(new FileServerOptions
 {
