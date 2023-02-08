1275b339bc269f63e26e8eb5766f1c65e608645e
Dmitriy Dubson <ddubson@vmware.com>
2023-02-08 15:53:16 -0500

    adds support for Spring Boot 3 in appsso-starter-java accelerator

--- appsso-starter-java/src/main/java/com/vmware/tanzu/apps/sso/accelerator/config/AuthenticationCheckFilter.java
+++ appsso-starter-java/src/main/java/com/vmware/tanzu/apps/sso/accelerator/config/AuthenticationCheckFilter.java
@@ -3,13 +3,12 @@ package com.vmware.tanzu.apps.sso.accelerator.config;
 import java.io.IOException;
 import java.util.stream.Stream;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
