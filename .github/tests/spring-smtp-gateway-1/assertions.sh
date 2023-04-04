2026a58c65f6c16e5b689ea474f457ed6be3f4d8
trisberg <risbergt@vmware.com>
2023-04-04 17:17:45 -0400

    Remove test assertions for rabbit files

--- .github/tests/spring-smtp-gateway-1/assertions.sh
+++ .github/tests/spring-smtp-gateway-1/assertions.sh
@@ -2,10 +2,6 @@
 set -euxo pipefail
 pushd $1
 
-# Check that RabbitMQ files exist
-test -f config/service-operator/rmqCluster.yaml
-test -f config/app-operator/rmqResourceClaim.yaml
-
 ./mvnw package
 
 popd
