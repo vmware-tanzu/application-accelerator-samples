5d1a96c5485a0d3a9c83ee16a491dd864fe0783f
Dmitriy Dubson <ddubson@vmware.com>
2023-04-05 11:30:52 -0400

    fix compilation issue with sso integration in angular-frontend accelerator

--- angular-frontend/src/app/app-routing.module.ts
+++ angular-frontend/src/app/app-routing.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateCustomerProfileComponent } from "./customer-profile/create-customer-profile.component";
 import { ListCustomerProfilesComponent } from "./customer-profile/list-customer-profiles.component";
+// StartSSOImports
 import {UserProfileComponent} from "./user-profile/user-profile.component";
-import { HomeComponent } from "./home.component";
+// EndSSOImports
 
 const routes: Routes = [
   { path: 'customer-profiles/list', component: ListCustomerProfilesComponent },
   { path: 'customer-profiles/create', component: CreateCustomerProfileComponent },
+  // StartSSORoutes
   { path: 'user-profile', component: UserProfileComponent },
+  // EndSSORoutes
   { path: '', redirectTo: '/customer-profiles/list', pathMatch: 'full' }
 ];
 
