532c00d83f99abcb34fca740a822acce2f72de72
trisberg <risbergt@vmware.com>
2023-04-12 12:27:38 -0400

    Add build/test step for angular-frontend

--- .github/tests/angular-frontend-1/assertions.sh
+++ .github/tests/angular-frontend-1/assertions.sh
@@ -5,4 +5,9 @@ pushd $1
 # Check that backendService name is used
 grep -q 'customer-profile-backend.namespace' nginx.conf
 
+npm install
+npm install -g @angular/cli
+ng build
+ng test --browsers=ChromeHeadless --watch=false
+
 popd
