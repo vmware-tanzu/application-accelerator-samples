406ae811634231d9d94a2413d853a7e7375dce07
Kris De Volder <kdvolder@vmware.com>
2023-05-09 15:03:17 -0700

    Tweak settings.gradle (replace project name)

--- .github/tests/tanzu-java-web-app-boot2/assertions.sh
+++ .github/tests/tanzu-java-web-app-boot2/assertions.sh
@@ -2,15 +2,11 @@
 set -euxo pipefail
 
 pushd $1
-if [ -f build.gradle.kts -o -f settings.gradle ]; then
+if [ -f build.gradle.kts ] || [ -f settings.gradle ]; then
   echo "Gradle files shouldn't exist"
   exit 1
 fi
-if [ -f build.gradle.kts -o -f settings.gradle ]; then
-  echo "Gradle files shouldn't exist"
-  exit 1
-fi
-if [ -e gradle -o -e gradlew -o -e gradlew.bat ]; then
+if [ -e gradle ] || [ -e gradlew ] || [ -e gradlew.bat ]; then
   echo "Gradle wrapper files shouldn't exist"
   exit 1
 fi
--- run-test-local.sh
+++ run-test-local.sh
@@ -3,22 +3,20 @@ set -euo pipefail
 
 export GITHUB_WORKSPACE=$(pwd)
 
-#test_name=tanzu-java-web-app-boot2
-#test_name=tanzu-java-web-app-boot3
-#test_name=tanzu-java-web-app-boot2_gradle
-test_name=tanzu-java-web-app-boot3_gradle
-
 docker pull dev.registry.tanzu.vmware.com/app-accelerator/acc-engine:latest
 acc_server=$(docker run -d -p 8888:8080 dev.registry.tanzu.vmware.com/app-accelerator/acc-engine:latest)
 finish() {
     docker stop $acc_server
 }
 trap finish EXIT ERR
-
 until $(curl --output /dev/null --silent --head --fail http://localhost:8888/actuator/health); do
   printf 'Waiting for acc server...'
   sleep 2
 done
 
-.github/tests/generate.sh $test_name
-.github/tests/run-test.sh $test_name
+pushd .github/tests
+for test_name in tanzu-java-*; do
+   ./generate.sh $test_name
+   ./run-test.sh $test_name
+done
+popd
