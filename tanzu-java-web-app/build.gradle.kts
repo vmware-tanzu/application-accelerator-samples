6878d277385269b7e20f2fc6c79b806797b8f145
Kris De Volder <kdvolder@vmware.com>
2023-05-09 10:23:25 -0700

    Add gradle build files

---
+++ tanzu-java-web-app/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("org.springframework.boot") version "2.7.10"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    id("java")
+}
+
+group = "com.vmware.tap.accelerators"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+extra["snakeyaml.version"] = "1.33"
+extra["spring-security.version"] = "5.8.2"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Spring
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+    // Test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.named<Jar>("jar") {
+    enabled = false
+}
