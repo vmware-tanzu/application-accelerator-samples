db7af8ea82c515f2badbae8eb904e0193d7de7bb
Kris De Volder <kdvolder@vmware.com>
2023-05-11 11:59:22 -0700

    Cleanup: create-acc.sh removed

--- create-acc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-tanzu acc delete kdv
-tanzu acc create kdv2 \
-  --git-repo https://github.com/kdvolder/application-accelerator-samples.git \
-  --git-branch tanzu-java-web-app-gradle \
-  --git-sub-path tanzu-java-web-app \
