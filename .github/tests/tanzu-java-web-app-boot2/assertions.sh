127a9c3d57b6e38b74e13964e5d03cacd4031d3c
trisberg <risbergt@vmware.com>
2023-05-15 17:43:22 -0400

    Update tests for nativeBuild option

--- .github/tests/tanzu-java-web-app-boot2/assertions.sh
+++ .github/tests/tanzu-java-web-app-boot2/assertions.sh
@@ -2,6 +2,13 @@
 set -euxo pipefail
 
 pushd $1
+# Check that native README is not included
+c=$(grep -c 'GraalVM' README.md || true)
+if [ $c -gt 0 ]; then
+  echo "GraalVM should not be in README.md"
+  exit 1
+fi  
+
 if [ -f build.gradle.kts ] || [ -f settings.gradle ]; then
   echo "Gradle files shouldn't exist"
   exit 1
