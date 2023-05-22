e79bf39e0b439a3b22f4f5eae4b5a4b12ff5cc62
trisberg <risbergt@vmware.com>
2023-05-22 17:23:25 -0400

    Update Spring Boot version

--- java-server-side-ui/build.gradle.kts
+++ java-server-side-ui/build.gradle.kts
@@ -1,6 +1,6 @@
 
 plugins {
-    id("org.springframework.boot") version "2.7.10"
+    id("org.springframework.boot") version "2.7.12"
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
     id("java")
 }
@@ -10,7 +10,6 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 extra["snakeyaml.version"] = "1.33"
-extra["spring-security.version"] = "5.8.2"
 
 repositories {
     mavenCentral()
