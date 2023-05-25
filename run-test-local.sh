6dc032e6fd2057b7a5d1b847b1ed592c206321a6
trisberg <risbergt@vmware.com>
2023-05-25 12:52:35 -0400

    Updated comments for run-test-lcoal.sh

--- run-test-local.sh
+++ run-test-local.sh
@@ -7,7 +7,6 @@ set -euo pipefail
 # export CLI_PLUGIN=acc
 # export TEST_PATTERN=tanzu-java-web-app-*  # set this to a pattern that matches the tests you want to run
 # export TEST_WORKSPACE=$PWD
-# export TEST_WORKSPACE=$PWD
 # export ACC_SERVER_URL=<URL>  # this is the URL for Tanzu Portal (TAP-GUI) in your view cluster
 
 PATTERN="${TEST_PATTERN:-*}"
