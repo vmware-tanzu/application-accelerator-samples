430ec374efcf35c1e4b2e67db5e6d8d2bdbacf2a
Eleftheria Stein <estein@vmware.com>
2023-05-25 17:51:44 +0200

    Fix tests for SSO samples

--- .github/tests/java-server-side-ui-gradle+sso/assertions.sh
+++ .github/tests/java-server-side-ui-gradle+sso/assertions.sh
@@ -8,10 +8,6 @@ test -f build.gradle.kts
 # Check that pom.xml does not exist
 test ! -f pom.xml
 
-# Check that SSO files exist
-test -f config/client-registration.yaml
-test -f config/client-registration-claim.yaml
-
 # Check that OIDC files exist
 test -f src/main/java/com/example/serversideuistarter/web/OidcUserController.java
 
