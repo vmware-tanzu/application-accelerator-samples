246aa953619bfc7d6a2fe4e5751cee23c51f2340
trisberg <risbergt@vmware.com>
2023-06-23 12:55:47 -0400

    Enable scanning of where-for-dinner modules

--- .github/tests/where-for-dinner-1/scan.sh
+++ .github/tests/where-for-dinner-1/scan.sh
@@ -7,6 +7,14 @@ grype=$2
 ./mvnw package
 
 $grype where-for-dinner-ui --fail-on high --config ${GITHUB_WORKSPACE}/.github/tests/.grype.yaml
-echo "TODO: Scan Java services when the following Grype issue (which produces many false positives) is resolved https://github.com/anchore/grype/issues/1009"
+$grype where-for-dinner-crawler-python --fail-on high --config ${GITHUB_WORKSPACE}/.github/tests/.grype.yaml
+echo "Scan Java services - depends on exclusions in .grype.yaml"
+$grype where-for-dinner-api-gateway/target/*.jar --fail-on high --config ${GITHUB_WORKSPACE}/.github/tests/.grype.yaml
+$grype where-for-dinner-availability/target/*.jar --fail-on high --config ${GITHUB_WORKSPACE}/.github/tests/.grype.yaml
+$grype where-for-dinner-crawler/target/*.jar --fail-on high --config ${GITHUB_WORKSPACE}/.github/tests/.grype.yaml
+$grype where-for-dinner-notify/target/*.jar --fail-on high --config ${GITHUB_WORKSPACE}/.github/tests/.grype.yaml
+$grype where-for-dinner-notify/target/*.jar --fail-on high --config ${GITHUB_WORKSPACE}/.github/tests/.grype.yaml
+$grype where-for-dinner-search/target/*.jar --fail-on high --config ${GITHUB_WORKSPACE}/.github/tests/.grype.yaml
+$grype where-for-dinner-search-proc/target/*.jar --fail-on high --config ${GITHUB_WORKSPACE}/.github/tests/.grype.yaml
 
 popd
