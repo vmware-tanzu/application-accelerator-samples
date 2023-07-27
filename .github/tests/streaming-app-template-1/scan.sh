e2d8c0340dacbe0b6b6d4cce928ee48fde7244eb
trisberg <risbergt@vmware.com>
2023-07-27 10:42:29 -0400

    Update to use spring-streaming-app name consistently

--- .github/tests/streaming-app-template-1/scan.sh
+++ .github/tests/streaming-app-template-1/scan.sh
@@ -5,6 +5,6 @@ pushd $1
 grype=$2
 
 ./mvnw package
-$grype target/streaming-app-template-0.0.1-SNAPSHOT.jar --fail-on high --config ${GITHUB_WORKSPACE}/.github/tests/.grype.yaml
+$grype target/spring-streaming-app-0.0.1-SNAPSHOT.jar --fail-on high --config ${GITHUB_WORKSPACE}/.github/tests/.grype.yaml
 
 popd
