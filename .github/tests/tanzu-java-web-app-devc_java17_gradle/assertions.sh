b0d7c246d374b20d73b29e5f68707acfd7bf324f
Kris De Volder <kdvolder@vmware.com>
2023-08-18 15:53:20 -0700

    Install maven/gradle in devcontainer based on acc options

--- .github/tests/tanzu-java-web-app-devc_java17_gradle/assertions.sh
+++ .github/tests/tanzu-java-web-app-devc_java17_gradle/assertions.sh
@@ -9,9 +9,9 @@ else
    if ! grep -q '"version": "17"' .devcontainer.json; then
       echo "Couldn't find Java version 17"
       exit 2
-   fi
-   if grep -q 'installGradle' .devcontainer.json; then
-      echo "devcontainer.json shouldn't have 'installGradle'"
+   fi 
+   if ! grep -q '"installGradle": true' .devcontainer.json; then
+      echo "Couldn't find 'installGradle'"
       exit 3
    fi
    if grep -q 'installMaven' .devcontainer.json; then
--- .github/tests/tanzu-java-web-app-devc_java17_maven/assertions.sh
+++ .github/tests/tanzu-java-web-app-devc_java17_maven/assertions.sh
@@ -9,13 +9,13 @@ else
    if ! grep -q '"version": "17"' .devcontainer.json; then
       echo "Couldn't find Java version 17"
       exit 2
+   fi 
+   if ! grep -q '"installMaven": true' .devcontainer.json; then
+      echo "Couldn't find 'installMaven'"
+      exit 3
    fi
    if grep -q 'installGradle' .devcontainer.json; then
       echo "devcontainer.json shouldn't have 'installGradle'"
-      exit 3
-   fi
-   if grep -q 'installMaven' .devcontainer.json; then
-      echo "devcontainer.json shouldn't have 'installMaven'"
       exit 4
    fi
 fi
