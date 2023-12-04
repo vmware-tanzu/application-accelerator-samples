d50e2841442c19102d895d3e9d59ef6ffec889b3
trisberg <risbergt@vmware.com>
2023-12-04 17:25:10 -0500

    Update to use Spring Boot 3.1.6
    Signed-off-by: trisberg <risbergt@vmware.com>

--- java-rest-service/build.gradle.kts
+++ java-rest-service/build.gradle.kts
@@ -1,7 +1,7 @@
 
 plugins {
-    id("org.springframework.boot") version "3.1.3"
-    id("io.spring.dependency-management") version "1.1.3"
+    id("org.springframework.boot") version "3.1.6"
+    id("io.spring.dependency-management") version "1.1.4"
     id("java")
 }
 
