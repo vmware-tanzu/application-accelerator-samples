220ebbdbb26a122ff4390257267b0968ea2280d7
Max Brauer <mbrauer@vmware.com>
2024-01-03 14:13:09 +0100

    fix: do not redirect to /home when accessing private pages
    instead let the user be redirected to the login page
    
    Signed-off-by: Max Brauer <mbrauer@vmware.com>

--- appsso-starter-java/src/main/java/com/vmware/tanzu/apps/sso/accelerator/config/WebSecurityConfig.java
+++ appsso-starter-java/src/main/java/com/vmware/tanzu/apps/sso/accelerator/config/WebSecurityConfig.java
@@ -12,7 +12,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -46,12 +45,7 @@ public class WebSecurityConfig {
 						logout.logoutSuccessUrl("/home");
 				})
 				.oauth2Login(withDefaults())
-				.oauth2Client(withDefaults())
-				.exceptionHandling(
-						// When the user is not logged in and is trying to access a private page,
-						// redirect to /home
-						exceptionHandling -> exceptionHandling.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/home"))
-				);
+				.oauth2Client(withDefaults());
 		return http.build();
 	}
 
