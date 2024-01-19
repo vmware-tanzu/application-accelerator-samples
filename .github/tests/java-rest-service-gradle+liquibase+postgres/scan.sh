7113e228a235a8506ab17b90f30d67c0aa73b426
trisberg <risbergt@vmware.com>
2024-01-19 14:51:44 -0500

    Skip tests when building for scan of project using testcontainers

--- .github/tests/java-rest-service-gradle+liquibase+postgres/scan.sh
+++ .github/tests/java-rest-service-gradle+liquibase+postgres/scan.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 pushd $1
 grype=$2
 
-./gradlew build
+./gradlew build -x test
 $grype build/libs/customer-profile-0.0.1-SNAPSHOT.jar --fail-on high --config ${GITHUB_WORKSPACE}/.github/tests/.grype.yaml
 
 popd
--- .github/tests/java-rest-service-maven+flyway+postgres/scan.sh
+++ .github/tests/java-rest-service-maven+flyway+postgres/scan.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 pushd $1
 grype=$2
 
-./mvnw package
+./mvnw package -DskipTests
 $grype target/customer-profile-0.0.1-SNAPSHOT.jar --fail-on high --config ${GITHUB_WORKSPACE}/.github/tests/.grype.yaml
 
 popd
