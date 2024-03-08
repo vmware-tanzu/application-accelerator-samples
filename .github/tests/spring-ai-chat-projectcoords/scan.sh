4299b9c55bb383b016f4a31e707bae5a24539e4f
trisberg <risbergt@vmware.com>
2024-03-08 18:57:04 -0500

    Fix artifact name used for scan

--- .github/tests/spring-ai-chat-projectcoords/scan.sh
+++ .github/tests/spring-ai-chat-projectcoords/scan.sh
@@ -5,6 +5,6 @@ pushd $1
 grype=$2
 
 ./mvnw package -DskipTests
-$grype target/coordstest-0.0.1-SNAPSHOT.jar --fail-on high --config ${GITHUB_WORKSPACE}/.github/tests/.grype.yaml
+$grype target/testcoords-0.0.1-SNAPSHOT.jar --fail-on high --config ${GITHUB_WORKSPACE}/.github/tests/.grype.yaml
 
 popd
