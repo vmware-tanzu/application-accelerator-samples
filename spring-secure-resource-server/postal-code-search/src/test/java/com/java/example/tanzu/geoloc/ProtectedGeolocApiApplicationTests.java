69edf5cb821bf2363141c186515ef25527686962
meyerg <meyerg@vmware.com>
2024-03-14 14:35:17 -0500

    Initial commit of spring secure resource server.

---
+++ spring-secure-resource-server/postal-code-search/src/test/java/com/java/example/tanzu/geoloc/ProtectedGeolocApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.example.tanzu.geoloc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtectedGeolocApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
