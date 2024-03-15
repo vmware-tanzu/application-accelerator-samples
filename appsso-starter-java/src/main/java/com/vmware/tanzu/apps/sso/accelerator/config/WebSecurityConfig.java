57d6ce128b55e247c0092bfab0d49ff21ee82b6c
trisberg <risbergt@vmware.com>
2024-03-15 15:22:53 -0400

    Remove unused imports for appsso-starter-java

--- appsso-starter-java/src/main/java/com/vmware/tanzu/apps/sso/accelerator/config/WebSecurityConfig.java
+++ appsso-starter-java/src/main/java/com/vmware/tanzu/apps/sso/accelerator/config/WebSecurityConfig.java
@@ -9,10 +9,8 @@ import org.springframework.context.annotation.DependsOn;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
