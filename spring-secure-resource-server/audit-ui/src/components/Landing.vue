dbb3f6118b989401730b66ba40cae230e75a819a
meyerg <meyerg@vmware.com>
2024-03-20 14:14:49 -0500

    Adding audit-ui app.
    Updating appSSO to use client registration in order to support multiple
    redirect URIs.
    
    Updating SCG routes with specific host names for UI apps.
    
    Removing full Workload ui in accelerator options and replacing with just
    the domain name.

---
+++ spring-secure-resource-server/audit-ui/src/components/Landing.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+  const login = () => {
+    let port = (window.location.port ? ':' + window.location.port : '');
+    window.location.href = '//' + window.location.hostname + port + '/auditViewer';
+  }
+</script>
+
+<template>
+   <main align="center">
+            <div  >
+              <h1 align="center">Location Audit Viewer</h1>                    
+            </div> <br>
+            <div align="center" className="container welcomeContent">
+                      The audit viewer list events occuring within the location system.
+            </div>                  
+            <div align="center" id="login">
+                  <button @click="login" >Login</button>
+            </div> 
+    </main>           
+</template>
---
+++ spring-secure-resource-server/audit-ui/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Landing from '../components/Landing.vue'
+</script>
+
+<template>
+  <main>
+    <Landing />
+  </main>
+</template>
