534521591351f7dc7027a8febe32b1cb05fba459
meyerg <meyerg@vmware.com>
2024-04-01 07:33:40 -0500

    Fixing unit test execustions for spring secure resoure server.

--- spring-secure-resource-server/audit/src/test/java/com/java/example/tanzu/audit/AuditApplicationTests.java
+++ spring-secure-resource-server/audit/src/test/java/com/java/example/tanzu/audit/AuditApplicationTests.java
@@ -3,7 +3,7 @@ package com.java.example.tanzu.audit;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.security.oauth2.resourceserver.jwt.issuer-uri=http://doesntmatter")
 class AuditApplicationTests {
 
 	@Test
