f3d02a4a71c4b9006a121e32e5a5abf747218e5d
trisberg <risbergt@vmware.com>
2024-04-10 16:06:23 -0400

    Remove outdated choices for Tanzu Java Web App
    - Update tests after options changes in tanzu-java-web-app

--- tanzu-java-web-app/src/main/java/com/example/springboot/Application.java
+++ tanzu-java-web-app/src/main/java/com/example/springboot/Application.java
@@ -7,8 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
-import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
+import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 
 @SpringBootApplication
 public class Application {
@@ -18,22 +18,7 @@ public class Application {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
-
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			for (String beanName : beanNames) {
-				System.out.println(beanName);
-			}
-
-		};
-	}
-	
-	@Bean
-	public HttpTraceRepository htttpTraceRepository() {
-		return new InMemoryHttpTraceRepository();
+	public HttpExchangeRepository htttpTraceRepository() {
+		return new InMemoryHttpExchangeRepository();
 	}
 }
