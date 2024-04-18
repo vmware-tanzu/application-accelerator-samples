a1ea0d2418d70f6e8878b23c1572eedcfe051737
trisberg <risbergt@vmware.com>
2024-04-18 10:05:36 -0400

    Remove unused imports

--- tanzu-java-web-app/src/main/java/com/example/springboot/Application.java
+++ tanzu-java-web-app/src/main/java/com/example/springboot/Application.java
@@ -1,11 +1,7 @@
 package com.example.springboot;
 
-import java.util.Arrays;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
 import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
