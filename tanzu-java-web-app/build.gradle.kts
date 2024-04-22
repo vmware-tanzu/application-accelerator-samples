8a84812b9c5fe20247de1400f3842a27125d92c9
Tom Kennedy <ktom@vmware.com>
2024-04-22 12:28:57 -0400

    Update java version in tanzu-java-web-app
    -  this was causing build failures when testing with tanzu build
    
    Signed-off-by: Tom Kennedy <ktom@vmware.com>

--- tanzu-java-web-app/build.gradle.kts
+++ tanzu-java-web-app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 group = "com.vmware.tap.accelerators"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 springBoot {
 	buildInfo()
