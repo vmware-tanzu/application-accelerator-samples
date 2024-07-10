14cc801f521f30d09ac5fedcf4e9e5a0db128ded
trisberg <risbergt@vmware.com>
2024-07-10 15:36:06 -0400

    Temporary disable grype scan for CVE-2024-29415 until we have a fix

--- .github/tests/angular-frontend-1/scan.sh
+++ .github/tests/angular-frontend-1/scan.sh
@@ -4,7 +4,6 @@ set -euxo pipefail
 pushd $1
 grype=$2
 
-# disable grype scan until we have a fix for CVE-2024-29415
-# $grype dir:. --fail-on high --config ${GITHUB_WORKSPACE}/.github/tests/.grype.yaml
+$grype dir:. --fail-on high --config ${GITHUB_WORKSPACE}/.github/tests/.grype.yaml
 
 popd
