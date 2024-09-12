9f4805e84790ee8c9e0bf784480dd73c83cac9f5
trisberg <thomas.risberg@broadcom.com>
2024-09-12 12:50:06 -0400

    Include tests for the first group of samples for TP for K8s

---
+++ .github/tests/tanzu-java-web-app-defaults/assertions.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+
+pushd $1
+
+if ls | grep -q gradle; then
+   echo "FAIL: There are gradle related files present."
+   exit 1
+fi
+
+if ! grep -q '<java.version>17</java.version>' pom.xml; then
+   echo "Couldn't find Java version 17"
+   exit 2
+fi
+
+./mvnw package
+
+popd
