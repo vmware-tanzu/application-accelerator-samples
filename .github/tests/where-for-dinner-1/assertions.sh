d2fef425563ef39a25963831554a4ba85aae5cb1
Thomas Risberg <thomas.risberg@broadcom.com>
2024-09-26 14:04:40 -0400

    Update where-for-dinner for TP with new DSL and removing TAP/TAS artifacts and instructions (#529)
    * Update to use Spring Boot 3.3.4
    
    * Remove TAP/TAS artifacts
    
    * Remove test for TAP artifacts
    
    * Remove Tiltfile and workload.yaml
    
    * Remove deployment docs and Backstage catalog
    
    * Remove TAP service options
    
    * remove engine section and add new DSL .axl file
    
    * remove manifest.yaml files

--- .github/tests/where-for-dinner-1/assertions.sh
+++ .github/tests/where-for-dinner-1/assertions.sh
@@ -2,26 +2,6 @@
 set -euxo pipefail
 pushd $1
 
-# Check that workloads path is rewritten
-test -f config/developer/workloads.yaml
-
-# Check that RabbitMQ files exist
-test -f config/service-operator/rmqCluster.yaml
-
-# Check that MySQL files don't exist
-test ! -f config/service-operator/mysqlInstance.yaml
-test ! -f config/app-operator/mysqlResourceClaim.yaml
-
-# Check that cloudevents files don't exist
-test ! -f config/app-operator/knEventing.yaml
-
-# Check that security files don't exist
-test ! -f config/service-operator/appSSOInstance.yaml
-
-# Check that redis files don't exist
-test ! -f config/service-operator/redisInstance.yaml
-test ! -f config/app-operator/redisResourceClaim.yaml
-
 ./mvnw package
 
 popd
