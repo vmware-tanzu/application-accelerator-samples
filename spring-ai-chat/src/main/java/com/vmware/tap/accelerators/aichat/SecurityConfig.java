9415cf9a29e30b67985c88ce8be6d3cd21857260
trisberg <thomas.risberg@broadcom.com>
2025-01-28 09:07:15 -0500

    Avoid use of deprecated method in spring-ai-chat

--- spring-ai-chat/src/main/java/com/vmware/tap/accelerators/aichat/SecurityConfig.java
+++ spring-ai-chat/src/main/java/com/vmware/tap/accelerators/aichat/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-            .authorizeRequests(authorize -> authorize
+            .authorizeHttpRequests(authorize -> authorize
                 .requestMatchers("/login").permitAll()
                 .anyRequest().authenticated()
             )
