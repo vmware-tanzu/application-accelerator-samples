8b8b3dba120999cb547136bbe9c0d49aa2566b8e
Craig Walls <cwalls@vmware.com>
2025-04-21 10:59:59 -0600

    Streaming WIP

--- python-ai-chat/main.py
+++ python-ai-chat/main.py
@@ -1,26 +1,98 @@
 import os
 import json
-from flask import Flask, request, render_template
+import queue
+import time
+from random import randint
+from flask import Flask, request, render_template, stream_with_context
 from langchain_openai import ChatOpenAI
 
 memory = []
 app = Flask(__name__)
+message_queue = queue.Queue()
 
 @app.route("/", methods=['GET'])
 def home():
     return render_template("index.html")
 
-@app.route("/ask", methods=['POST'])
-def ask():
-    apiModelDetails = getModelApiDetails()
-    llm = ChatOpenAI(base_url=apiModelDetails['apiBase'], model=apiModelDetails['modelName'], api_key=apiModelDetails['apiKey'])
-    message = request.form['message']
-    promptMessages = memory.copy()
-    promptMessages.append({ 'role':'user', 'content': message })
-    answer = llm.invoke(promptMessages)
-    storeInMemory(message, answer.content)
-    model = { 'messageIn': message, 'messageOut': answer.content }
-    return render_template("chatMessage.html", model=model)
+@app.route('/ask', methods=['POST'])
+def chat_input():
+    print("=========== STARTING ASK FLOW ============")
+    user_input = request.form.get("message")
+    # current_response_id = f"gptblock{randint(67,999999)}"
+    print("-----> PUTTING USER MESSAGE INTO QUEUE")
+    message_queue.put(user_input)
+    print(f"-----> QUEUE SIZE {message_queue.qsize()}")
+
+    userMessageTemplate = render_template('userMessage.html', user_message=user_input)
+    userRes = f"""data: {userMessageTemplate}\n\n"""
+    yield userRes
+
+    return "Success", 204
+
+@app.route('/stream')
+def stream():
+    def message_stream():
+        global new_conversation
+
+        while True:
+            if not message_queue.empty():
+                print(".")
+
+                print("----->  MESSAGE QUEUE NOT EMPTY")
+                user_message = message_queue.get()
+                userMessageTemplate = render_template('userMessage.html', user_message=user_message)
+                userRes = f"""data: {userMessageTemplate}\n\n"""
+
+
+                ### PROBLEM: At this yield point, sometimes it doesn't get past the yield. E.g., no "YIELDED"
+                ###          message is ever logged. But sometimes it does move past it, but the browser never
+                ###          receives anything in the stream.
+                print(f"-----> YIELDING : {userRes}")
+                yield userRes
+                print("-----> YIELDED")
+                print(f"-----> USER MESSAGE {user_message}")
+
+                apiModelDetails = getModelApiDetails()
+                print("-----> A")
+                llm = ChatOpenAI(base_url=apiModelDetails['apiBase'], model=apiModelDetails['modelName'], api_key=apiModelDetails['apiKey'])
+                print("-----> B")
+                # message = request.form['message']
+                promptMessages = memory.copy()
+                print("-----> C")
+                promptMessages.append({ 'role':'user', 'content': user_message })
+                print("-----> D")
+                stream = llm.stream(promptMessages)
+                print("-----> Got a stream response...")
+                # storeInMemory(message, answer.content)
+
+
+
+                current_response_id = f"getblock{randint(67, 999999)}"
+                print("-----> E")
+                message = ""
+                hx_swap = False
+                print("-----> F")
+                for chunk in stream:
+                    print("-----> G")
+                    try:
+                        word = chunk.content
+                        message += word.replace("\n", "<br>")
+                        ai_message = message
+                        # res = f"""data: <div class="mb-8" id="{current_response_id}" {"hx-swap-oob='true'" if hx_swap else ""}><div class="inline-block bg-red-300 text-stone-950 rounded-lg p-2 ml-auto"><b>{ user_message }</b></div><p class="mt-4 h-full overflow-auto">{ai_message}</p></div>\n\n"""
+                        res = f"""data: <p class="mt-4 overflow-auto" id="{current_response_id}" {"hx-swap-oob='true'" if hx_swap else ""}>{ai_message}</p>\n\n"""
+                        hx_swap = True
+
+                        ### PROBLEM: At this yield point, the code continues to run (e.g., you always get a "YIELDED" message)
+                        ###          But the browser doesn't seem to receive anything in its stream.
+                        print(f"-----> YIELDING PARTIAL RESPONSE {res}")
+                        yield res
+                        print(f"-----> YIELDED")
+                    except Exception as e:
+                        print(e)
+                        return e
+                print("======= ENDING ASK FLOW =======\n\n")
+                    
+    return app.response_class(stream_with_context(message_stream()), mimetype="text/event-stream")
 
 @app.route("/health")
 def health():
