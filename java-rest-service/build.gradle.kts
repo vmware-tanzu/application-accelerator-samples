8b65e1ecc60b48cf1a24e237867fd744e956797b
trisberg <thomas.risberg@broadcom.com>
2025-08-05 15:00:23 -0400

    Update samples to use Boot 3.5.4

--- java-rest-service/build.gradle.kts
+++ java-rest-service/build.gradle.kts
@@ -1,6 +1,6 @@
 
 plugins {
-    id("org.springframework.boot") version "3.5.0"
+    id("org.springframework.boot") version "3.5.4"
     id("io.spring.dependency-management") version "1.1.7"
     id("java")
 }
